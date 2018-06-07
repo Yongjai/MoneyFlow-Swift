a448415d285a421cc4071352a025f2dcd69a34e7
Yongjai <dev.yongjai@gmail.com>
2018-06-07 21:34:56 +0900

    fixed bug: can't load RealmSwift

---
+++ MoneyFlow-Swift/MoneyFlow-Swift/Source/Model/Asset.swift
@@ -0,0 +1,12 @@
+//
+//  Asset.swift
+//  MoneyFlow-Swift
+//
+//  Created by Yong Jae Kwon on 2018. 6. 7..
+//  Copyright © 2018년 Yong Jae Kwon. All rights reserved.
+//
+
+import RealmSwift
+
+
+
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 target ‘MoneyFlow-Swift’ do
-pod 'Realm', '~> 3.7.0'
+pod 'RealmSwift'
 project 'MoneyFlow-Swift/MoneyFlow-Swift.xcodeproj'
 use_frameworks!
 
